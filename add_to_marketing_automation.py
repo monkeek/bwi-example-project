067fc077b0df2632ed22f65d4bbcb87c54eaf465
Antoine MARTIN <antoine@visiblee.io>
2019-11-26 14:09:45 +0100

    Add signup email and add to marketing automation

--- add_to_marketing_automation.py
+++ add_to_marketing_automation.py
@@ -1,6 +1,37 @@
 #!/usr/bin/python
+import json
+import os
+
+import bwi
+from mailjet_rest import Client
+
+
 def callback_customer(data):
-    print("It's a good day, we've received a message in queue add_to_marketing_automation")
-    print(data)
-    print("Send it to queue billing now !")
+    data = (json.loads(data))
+    api_key = os.environ.get('MAILJET_APIKEY')
+    api_secret = os.environ.get('MAILJET_APISECRET')
+    mailjet = Client(auth=(api_key, api_secret), version='v3')
+    mjdata = {
+        "Action": "addnoforce",
+        'Contacts': [
+            {
+                "Email": data['email'],
+                "Name": data['first_name']
+            }
+        ]
+    }
+    listid = os.environ.get('MAILJET_MA_LIST')
+    result = mailjet.contactslist_managemanycontacts.create(data=mjdata,
+                                                            id=listid)
+    bwi.logs.info("Adding " + data['email'] + " to the list id=" + listid)
+    if 200 <= result.status_code <= 299:
+        bwi.logs.info(data['email'] + " has been added to the list")
+    else:
+        bwi.logs.error(
+            data['email'] + " can't be added to the list : " + result)
     return data
+
+
+if __name__ == '__main__':
+    callback_customer(
+        '{"email": "martin_e@etna-alternance.net", "first_name": "Antoine"}')
--- signup.py
+++ signup.py
@@ -1,6 +1,47 @@
 #!/usr/bin/python
+import json
+import os
+
+import bwi
+from mailjet_rest import Client
+
+
 def callback_customer(data):
-    print("It's a good day, we've received a message in queue signup")
-    print(data)
-    print("Send it to queue add_to_marketing_automation now !")
+    data = (json.loads(data))
+    api_key = os.environ.get('MAILJET_APIKEY')
+    api_secret = os.environ.get('MAILJET_APISECRET')
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+    mjdata = {
+        'Messages': [
+            {
+                "From": {
+                    "Email": "contact@bwi-project.com",
+                    "Name": "Antoine de BWI"
+                },
+                "To": [
+                    {
+                        "Email": data['email'],
+                        "Name": data['first_name']
+                    }
+                ],
+                "Subject": "Bienvenue chez BWI.",
+                "HTMLPart": "<h3>Bienvenue dans la hive !</h3><br/>May the "
+                            "force bee with you!"
+            }
+        ]
+    }
+    result = mailjet.send.create(data=mjdata)
+    bwi.logs.info("Sending signup email to " + data['email'])
+    if 200 <= result.status_code <= 299:
+        bwi.logs.info("Signup email has been sent to " + data['email'])
+        bwi.metrics.store("sent_email", 1)
+    else:
+        bwi.logs.error("Signup email has not been sent to " + data['email'])
+        bwi.metrics.store("error_email", 1)
+
     return data
+
+
+if __name__ == '__main__':
+    callback_customer(
+        '{"email": "antoine@visiblee.io", "first_name": "Antoine"}')
